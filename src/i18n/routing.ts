eac4e050d0c4cba3d7fc82fe076e3f3531613c3b
portfolio <adrianoopereiira@hotmail.com>
2025-07-14 21:26:19 -0300

    feat(i18n): add translation for pathnames

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,6 +5,35 @@ export const routing = defineRouting({
   locales: ['en', 'pt', 'es'],
   defaultLocale: 'pt',
   localeDetection: true,
+  localePrefix: 'always',
+
+  pathnames: {
+    '/feed': {
+      en: '/feed',
+      pt: '/feed',
+      es: '/feed'
+    },
+    '/privacy-policy': {
+      pt: '/politica-de-privacidade',
+      es: '/politica-de-privacidad'
+    },
+    '/cookie-policy': {
+      pt: '/politica-de-cookies',
+      es: '/politica-de-cookies'
+    },
+    '/terms-of-service': {
+      pt: '/termos-de-servico',
+      es: '/terminos-del-servicio'
+    },
+    '/create-account': {
+      pt: '/criar-conta',
+      es: '/crear-cuenta'
+    },
+    '/login': {
+      pt: '/entrar',
+      es: '/iniciar-sesion'
+    }
+  }
 })
 
 export type Locale = (typeof routing.locales)[number]
